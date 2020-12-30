46eeb02b1500cf591f1645b19d9ab15e004493e2
Rob Romito <rob@robertromito.com>
2020-12-30 09:48:40 -0500

    Created Spring Boot project

---
+++ backend/src/main/java/com/robertromito/foodvariety/FoodvarietyApplication.java
@@ -0,0 +1,13 @@
+package com.robertromito.foodvariety;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodvarietyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodvarietyApplication.class, args);
+	}
+
+}
